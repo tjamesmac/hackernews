62ea9e05570cd0e903e11a8a32a1eeffa8ec2d55
Thomas McAuliffe <tjamc@amazon.co.uk>
2023-01-31 14:59:53 +0000

    update: add extra data to the homepage

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,20 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 
-const API_BASE = "https://hacker-news.firebaseio.com/v0";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
 
-type Item = any;
+const API_BASE = "https://hacker-news.firebaseio.com/v0";
 
 export const handler: Handlers<any> = {
   async GET(_, ctx) {
     const resp = await fetch(`${API_BASE}/topstories.json`);
+
     if (!resp.ok) {
       const body = await resp.text();
       throw new Error(`${resp.status} ${body}`);
     }
+
     const itemIds = Object.values(await resp.json()).slice(0, 30) as number[];
     const items = await Promise.all(
       itemIds.map((id) => fetchItem(id)),
@@ -20,10 +23,40 @@ export const handler: Handlers<any> = {
   },
 };
 
+type Comments = any;
+
+type Item = {
+  id: string;
+  user: string;
+  points: number;
+  time: Date;
+  content: string;
+  url: string;
+  type: string; // TODO: perhaps a certain type
+  title: string;
+  comments_count: number;
+  comments?: Comments[];
+};
+
+type IncomingResp = {
+  id: string;
+  by: string;
+  score: number;
+  time: Date;
+  text: string;
+  url: string;
+  type: string; // TODO: perhaps a certain type
+  title: string;
+  comments_count: number;
+  kids: {
+    id: number;
+  };
+};
+
 export async function fetchItem(
   id: number,
   withComments = false,
-): Promise<any> {
+): Promise<Item> {
   const resp = await fetch(
     `${API_BASE}/item/${id}.json`,
   );
@@ -32,7 +65,7 @@ export async function fetchItem(
     throw new Error(`${resp.status} ${body}`);
   }
 
-  const item = await resp.json() as any;
+  const item = await resp.json() as IncomingResp;
   item.kids = item.kids || [];
   return {
     id: item.id,
@@ -52,6 +85,24 @@ export async function fetchItem(
   };
 }
 
+export default function Home(props: PageProps<Item[]>) {
+  const { data } = props;
+  return (
+    <>
+      <Head>
+        <title>The News</title>
+      </Head>
+      <Header active="/" />
+      <div class="p-4 mx-auto max-w-screen-lg">
+        <ul class="flex flex-col gap-1">
+          {data.map((item: Item) => <NewsItem item={item} />)}
+        </ul>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
 export function isAbsolute(url: string) {
   return /^https?:\/\//.test(url);
 }
@@ -60,22 +111,13 @@ export function getUrl(item: Item) {
   return item.url && isAbsolute(item.url) ? item.url : `/item?id=${item.id}`;
 }
 
-export default function Home(props: PageProps<any>) {
-  const { data } = props;
+export function NewsItem({ item }: { item: Item }) {
   return (
-    <>
-      <Head>
-        <title>The News</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-lg">
-        <ul class="flex flex-col gap-1">
-          {data.map((item) => (
-            <li class="text-2xl p-1 hover:text-blue-700">
-              <a href={getUrl(item)}>{item.title}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <li class="text-1xl p-2 hover:text-blue-700">
+      {item.points}
+      <a class="p-2" href={getUrl(item)}>{item.title}</a>{" "}
+      <span class="text-gray-400">({item.url.split("/")[2]})</span> {item.user}
+      {item.comments_count}
+    </li>
   );
 }
