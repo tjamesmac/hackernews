ea7088f1ea6b4ff0fea4d9cf6c3f0df5823b3d99
Thomas McAuliffe <tjamesmac@gmail.com>
2023-03-19 12:59:36 +0000

    Update text to blue

--- routes/index.tsx
+++ routes/index.tsx
@@ -115,7 +115,7 @@ export function getUrl(item: Item) {
 export function NewsItem({ item }: { item: Item }) {
   return (
     <li class="text-1xl p-2 hover:text-blue-700">
-      <span class="text-slate-300">{item.points}</span>
+      <span class="text-blue-300">{item.points}</span>
       <a class="p-2" href={getUrl(item)}>{item.title}</a>{" "}
       <span class="text-slate-300">{item.user}</span>
       <span class="text-slate-300">{item.comments_count}</span>
